dbe1ac82138c7fd176ac7fe5f530849ddfa25346
eesheesh <lironn@google.com>
2016-02-25 20:56:41 +0000

    Better logging, code cleanup, import summary.

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 #Import .mbox files to Google Apps for Work
 
-This script allows Google Apps admins to import mbox files in bulk for their users.
+This script allows Google Apps admins to import mbox files in bulk for their
+users.
 
 **DISCLAIMER**: This is not an official Google product.
 
 If you want to migrate from Mozilla Thunderbird, try
 [mail-importer](https://github.com/google/mail-importer).
 
-You only authorize it once using a service account, and then it can import mail into the
-mailboxes of all users in your domain.
+You only authorize it once using a service account, and then it can import mail
+into the mailboxes of all users in your domain.
 
 ###A. Creating and authorizing a service account for Gmail API
 
@@ -17,8 +18,8 @@ and log in as a domain super administrator.
 
 2. Create a new project.
 
- * If you have not used the API console before, select **Create a project** from the
-   **Select a project** dropdown list.
+ * If you have not used the API console before, select **Create a project** from
+   the **Select a project** dropdown list.
  * If this is not your first project, use the **Create Project** button.
 
 3. Enter "Gmail API" (or any name you prefer) as the project name and press the
@@ -27,8 +28,8 @@ Service at this point.
 
 4. Click the **Enable and manage APIs** link in the **Use Google APIs** box. 
 
-5. Enable the Gmail API - Select the **Gmail API** link and press the **Enable API**
-button. You can leave the default APIs enabled - it doesn't matter.
+5. Enable the Gmail API - Select the **Gmail API** link and press the **Enable
+   API** button. You can leave the default APIs enabled - it doesn't matter.
 
 6. Click the 3-line icon (**≡**) in the top left corner of the console.
 
@@ -38,16 +39,17 @@ button. You can leave the default APIs enabled - it doesn't matter.
 
 9. Enter a name (for example, "import-mailbox") in the **Name** field.
 
-10. Check the **Furnish a new private key** box and ensure the key type is set to JSON.
+10. Check the **Furnish a new private key** box and ensure the key type is set
+    to JSON.
 
-11. Check the **Enable Google Apps Domain-wide Delegation** box and enter a name in the
-**Product name for the consent screen** field.
+11. Check the **Enable Google Apps Domain-wide Delegation** box and enter a name
+    in the **Product name for the consent screen** field.
 
-12. Click **Create**. You will see a confirmation message advising that the Service
-account JSON file has been downloaded to your computer. Make a note of the location and
-name of this file. **This JSON file contains a private key that potentially allows access
-to all users in your domain. Protect it like you'd protect your admin password. Don't
-share it with anyone.**
+12. Click **Create**. You will see a confirmation message advising that the
+    Service account JSON file has been downloaded to your computer. Make a note
+    of the location and name of this file. **This JSON file contains a private
+    key that potentially allows access to all users in your domain. Protect it
+    like you'd protect your admin password. Don't share it with anyone.**
 
 13. Click **Close**.
 
@@ -55,8 +57,9 @@ share it with anyone.**
 
 15. Copy the **Client ID** value. You will need this later.
 
-16. Go to [the **Manage API client access** page of the Admin console for your Google
-Apps domain](https://admin.google.com/AdminHome?chromeless=1#OGX:ManageOauthClients).
+16. Go to [the **Manage API client access** page of the Admin console for your
+    Google Apps domain]
+    (https://admin.google.com/AdminHome?chromeless=1#OGX:ManageOauthClients).
 
 17. Under **Client Name**, enter the Client ID collected in step 15.
 
@@ -66,21 +69,22 @@ Apps domain](https://admin.google.com/AdminHome?chromeless=1#OGX:ManageOauthClie
    ```
 19. Click **Authorize**.
 
-You can now use the JSON file to authorize programs to access the Gmail API "insert" and
-"label" scopes of all users in your Google Apps domain.
+You can now use the JSON file to authorize programs to access the Gmail API
+"insert" and "label" scopes of all users in your Google Apps domain.
 
 ###B. Importing mbox files using import-mailbox-to-gmail.py
 
 1. Download the script - [import-mailbox-to-gmail.py]
 (https://github.com/google/import-mailbox-to-gmail/releases/download/v1.0/import-mailbox-to-gmail.py)
 
-2. [Download](https://www.python.org/downloads/) and install Python 2.7 (not Python 3.x)
-for your operating system if needed.
+2. [Download](https://www.python.org/downloads/) and install Python 2.7 (not
+   Python 3.x) for your operating system if needed.
 
-3. Open a **Command Prompt** (CMD) window (on Windows) / **Terminal** window (on Linux).
+3. Open a **Command Prompt** (CMD) window (on Windows) / **Terminal** window
+   (on Linux).
 
-4. Install the Google API Client Libraries for Python and their dependencies by running,
-   all in one line:
+4. Install the Google API Client Libraries for Python and their dependencies by
+   running, all in one line:
 
    Mac/Linux:
    ```
@@ -92,19 +96,19 @@ for your operating system if needed.
    C:\Python27\Scripts\pip install --upgrade google-api-python-client PyOpenSSL
    ```
 
-   Note: On Windows, you may need to do this on a Command Prompt window that was
-run as Administrator.
+   **Note**: On Windows, you may need to do this on a Command Prompt window that
+   was run as Administrator.
 
 5. Create a folder for the mbox files, for example `C:\mbox`.
 
 6. Under that folder, create a folder for each of the users into which you
-intend to import the mbox files. The folder names should be the users' full
-email addresses.
+   intend to import the mbox files. The folder names should be the users' full
+   email addresses.
 
 7. Into each of the folders, copy the mbox files for that user. Make sure the
-file name format is &lt;LabelName&gt;.mbox. For example, if you want the messages to
-go into a label called "Imported messages", name the file
-"Imported messages.mbox".
+   file name format is &lt;LabelName&gt;.mbox. For example, if you want the
+   messages to go into a label called "Imported messages", name the file
+   "Imported messages.mbox".
 
   Your final folder and file structure should look like this (for example):
   ```C:\mbox
@@ -121,18 +125,20 @@ go into a label called "Imported messages", name the file
   the messages are imported correctly.
 
 8. To start the migration, run the following command (one line):
-    Mac/Linux:
+   Mac/Linux:
    ```
-   ./import-mailbox-to-gmail.py --json Credentials.json --dir C:\mbox --replaceqp >> import-mbox-to-gmail.log 2>&1
+   ./import-mailbox-to-gmail.py --json Credentials.json --dir C:\mbox
    ```
 
    Windows:
    ```
-   C:\Python27\python import-mailbox-to-gmail.py --json Credentials.json --dir C:\mbox --replaceqp >> import-mbox-to-gmail.log 2>&1
+   C:\Python27\python import-mailbox-to-gmail.py --json Credentials.json --dir C:\mbox
    ```
 
-  * Replace `Credentials.json` with the path to the JSON file from step 12 above.
+  * Replace `Credentials.json` with the path to the JSON file from step 12
+    above.
   * Replace `C:\mbox` with the path to the folder you created in step 5.
 
 The mbox files will now be imported, one by one, into the users' mailboxes. You
-can monitor the migration by viewing the import-mbox-to-gmail.log file.
+can monitor the migration by looking at the output, and inspect errors by
+viewing the `import-mailbox-to-gmail.log` file.
--- import-mailbox-to-gmail.py
+++ import-mailbox-to-gmail.py
@@ -22,17 +22,29 @@ import base64
 import io
 import json
 import logging
+import logging.handlers
 import mailbox
 import os
 
 from apiclient import discovery
+from apiclient.http import set_user_agent
 import httplib2
 from apiclient.http import MediaIoBaseUpload
 from oauth2client.client import SignedJwtAssertionCredentials
+import oauth2client.tools
+import OpenSSL  # Required by Google API library, but not checked by it
+
+APPLICATION_NAME = 'import-mailbox-to-gmail'
+APPLICATION_VERSION = '1.1'
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.insert',
+          'https://www.googleapis.com/auth/gmail.labels']
+
 
 parser = argparse.ArgumentParser(
     description='Import mbox files to a specified label for many users.',
     formatter_class=argparse.RawDescriptionHelpFormatter,
+    parents=[oauth2client.tools.argparser],
     epilog=
     """
  * The directory needs to have a subdirectory for each user (with the full
@@ -62,28 +74,38 @@ parser.add_argument(
     dest='fix_msgid',
     required=False,
     action='store_false',
-    help="Don't fix Message-ID headers that are missing brackets "
+    help=
+    "Don't fix Message-ID headers that are missing brackets "
     "(default: fix them)")
 parser.add_argument(
-    '--replaceqp',
+    '--noreplaceqp',
     dest='replace_quoted_printable',
     required=False,
-    action='store_true',
+    action='store_false',
     help=
     "Replace 'Content-Type: text/quoted-printable' with text/plain (default: "
-    "don't change it)")
+    "replace it)")
 parser.add_argument(
     '--num_retries',
     default=10,
     type=int,
     help=
     'Maximum number of exponential backoff retries for failures (default: 10)')
-parser.set_defaults(fix_msgid=True, replace_quoted_printable=False)
+parser.add_argument(
+    '--log',
+    required=False,
+    default='%s-%d.log' % (APPLICATION_NAME, os.getpid()),
+    help=
+    'Optional: Path to a the log file (default: %s-####.log in the current '
+    'directory, where #### is the process ID)' % APPLICATION_NAME)
+parser.add_argument(
+    '--httplib2debuglevel',
+    default=0,
+    type=int,
+    help='Debug level of the HTTP library: 0=None (default), 4=Maximum.')
+parser.set_defaults(fix_msgid=True, replace_quoted_printable=True,
+                    logging_level='INFO')
 args = parser.parse_args()
-
-SCOPES = ['https://www.googleapis.com/auth/gmail.insert',
-          'https://www.googleapis.com/auth/gmail.labels']
-APPLICATION_NAME = 'Google Apps Gmail mbox importer'
 
 
 def get_credentials(username):
@@ -121,33 +143,165 @@ def get_label_id_from_name(service, username, labels, labelname):
         body=label_object).execute(num_retries=args.num_retries)
     logging.info("Label '%s' created", labelname)
     return label['id']
-  except:
-    logging.exception("Can't create label '%s' for user %s", labelname,
-                      username)
+  except Exception:
+    logging.error("Can't create label '%s' for user %s", labelname, username)
     raise
 
 
+def process_mbox_files(username, service, labels):
+  """Iterates over the mbox files found in the user's subdir and imports them.
+
+  Args:
+    username: The email address of the user to import into.
+    service: A Gmail API service object.
+    labels: Dictionary of the user's labels and their IDs.
+  Returns:
+    A tuple of: Number of labels imported without error,
+                Number of labels imported with some errors,
+                Number of labels that failed completely,
+                Number of messages imported without error,
+                Number of messages that failed.
+  """
+  number_of_labels_imported_without_error = 0
+  number_of_labels_imported_with_some_errors = 0
+  number_of_labels_failed = 0
+  number_of_messages_imported_without_error = 0
+  number_of_messages_failed = 0
+  for filename in os.listdir(os.path.join(args.dir, username)):
+    labelname, ext = os.path.splitext(filename)
+    full_filename = os.path.join(args.dir, username, filename)
+    if ext != '.mbox':
+      logging.info("Skipping '%s' because it doesn't have a .mbox extension",
+                   full_filename)
+      continue
+    logging.info("Starting processing of '%s'", full_filename)
+    number_of_successes_in_label = 0
+    number_of_failures_in_label = 0
+    mbox = mailbox.mbox(full_filename)
+    label_id = get_label_id_from_name(service, username, labels, labelname)
+    logging.info("Using label name '%s', ID '%s'", labelname, label_id)
+    for index, message in enumerate(mbox):
+      logging.info("Processing message %d in label '%s'", index,
+                    labelname)
+      try:
+        if (args.replace_quoted_printable and
+            'Content-Type' in message and
+            'text/quoted-printable' in message['Content-Type']):
+          message.replace_header(
+              'Content-Type', message['Content-Type'].replace(
+                  'text/quoted-printable', 'text/plain'))
+          logging.info('Replaced text/quoted-printable with text/plain')
+      except Exception:
+        logging.exception(
+            'Failed to replace text/quoted-printable with text/plain '
+            'in Content-Type header')
+      try:
+        if args.fix_msgid and 'Message-ID' in message:
+          msgid = message['Message-ID']
+          if msgid[0] != '<':
+            msgid = '<' + msgid
+            logging.info('Added < to Message-ID: %s', msgid)
+          if msgid[-1] != '>':
+            msgid += '>'
+            logging.info('Added > to Message-ID: %s', msgid)
+          message.replace_header('Message-ID', msgid)
+      except Exception:
+        logging.exception('Failed to fix brackets in Message-ID header')
+      try:
+        metadata_object = {'labelIds': [label_id]}
+        # Use media upload to allow messages more than 5mb.
+        # See https://developers.google.com/api-client-library/python/guide/media_upload
+        # and http://google-api-python-client.googlecode.com/hg/docs/epy/apiclient.http.MediaIoBaseUpload-class.html.
+        message_data = io.BytesIO(message.as_string().encode('utf-8'))
+        media = MediaIoBaseUpload(message_data,
+                                  mimetype='message/rfc822')
+        message_response = service.users().messages().import_(
+            userId=username,
+            fields='id',
+            neverMarkSpam=True,
+            processForCalendar=False,
+            internalDateSource='dateHeader',
+            body=metadata_object,
+            media_body=media).execute(num_retries=args.num_retries)
+        number_of_successes_in_label += 1
+        logging.debug("Imported mbox message '%s' to Gmail ID %s",
+                      message.get_from(), message_response['id'])
+      except Exception:
+        number_of_failures_in_label += 1
+        logging.exception('Failed to import mbox message')
+    logging.info("Finished processing '%s'. %d messages imported successfully, "
+                 "%d messages failed.",
+                 full_filename,
+                 number_of_successes_in_label,
+                 number_of_failures_in_label)
+    if number_of_failures_in_label == 0:
+      number_of_labels_imported_without_error += 1
+    elif number_of_successes_in_label > 0:
+      number_of_labels_imported_with_some_errors += 1
+    else:
+      number_of_labels_failed += 1
+    number_of_messages_imported_without_error += number_of_successes_in_label
+    number_of_messages_failed += number_of_failures_in_label
+  return (number_of_labels_imported_without_error,     # 0
+          number_of_labels_imported_with_some_errors,  # 1
+          number_of_labels_failed,                     # 2
+          number_of_messages_imported_without_error,   # 3
+          number_of_messages_failed)                   # 4
+
+
 def main():
-  """Iterates over the mbox files found in subdir and imports them into Gmail.
+  """Import multiple users' mbox files to Gmail.
 
   """
+  httplib2.debuglevel = args.httplib2debuglevel
+  # Use args.logging_level if defined.
+  try:
+    logging_level = args.logging_level
+  except AttributeError:
+    logging_level = 'INFO'
+
+  # Default logging to standard output
   logging.basicConfig(
-      level='INFO',
-      format=('%(asctime)s %(process)d %(levelname)s %(funcName)s '
-              '(%(filename)s:%(lineno)d) %(message)s'),
-      datefmt='%Y-%m-%dT%H:%M:%S (%z)')
+      level=logging_level,
+      format='%(asctime)s %(levelname)s %(funcName)s@%(filename)s %(message)s',
+      datefmt='%H:%M:%S')
+
+  # More detailed logging to file
+  file_handler = logging.handlers.RotatingFileHandler(args.log,
+                                                      maxBytes=1024 * 1024 * 32,
+                                                      backupCount=8)
+  file_formatter = logging.Formatter(
+      '%(asctime)s %(process)d %(levelname)s %(funcName)s '
+      '(%(filename)s:%(lineno)d) %(message)s')
+  file_formatter.datefmt = '%Y-%m-%dT%H:%M:%S (%z)'
+  file_handler.setFormatter(file_formatter)
+  logging.getLogger().addHandler(file_handler)
+
+  logging.info('*** Starting %s %s ***', APPLICATION_NAME, APPLICATION_VERSION)
   logging.info('Arguments:')
   for arg, value in sorted(vars(args).items()):
-      logging.info('\t%s: %r', arg, value)
+    logging.info('\t%s: %r', arg, value)
+
+  number_of_labels_imported_without_error = 0
+  number_of_labels_imported_with_some_errors = 0
+  number_of_labels_failed = 0
+  number_of_messages_imported_without_error = 0
+  number_of_messages_failed = 0
+  number_of_users_imported_without_error = 0
+  number_of_users_imported_with_some_errors = 0
+  number_of_users_failed = 0
+
   for username in next(os.walk(args.dir))[1]:
     try:
       logging.info('Processing user %s', username)
       try:
         credentials = get_credentials(username)
-        http = credentials.authorize(httplib2.Http())
+        http = credentials.authorize(set_user_agent(
+            httplib2.Http(),
+            '%s-%s' % (APPLICATION_NAME, APPLICATION_VERSION)))
         service = discovery.build('gmail', 'v1', http=http)
-      except:
-        logging.exception("Can't get access token for user %s", username)
+      except Exception:
+        logging.error("Can't get access token for user %s", username)
         raise
 
       try:
@@ -155,89 +309,59 @@ def main():
             userId=username,
             fields='labels(id,name)').execute(num_retries=args.num_retries)
         labels = results.get('labels', [])
-      except:
-        logging.exception("Can't get labels for user %s", username)
+      except Exception:
+        logging.error("Can't get labels for user %s", username)
         raise
 
       try:
-        for filename in os.listdir(os.path.join(args.dir, username)):
-          labelname, ext = os.path.splitext(filename)
-          full_filename = os.path.join(args.dir, username, filename)
-          if ext == '.mbox':
-            logging.info("Starting processing of '%s'", full_filename)
-            mbox = mailbox.mbox(full_filename)
-            label_id = get_label_id_from_name(service, username, labels,
-                                              labelname)
-            logging.info("Using label name '%s', ID '%s'", labelname, label_id)
-            for index, message in enumerate(mbox):
-              logging.info("Processing message %d in label '%s'", index,
-                           labelname)
-              try:
-                if (args.replace_quoted_printable and
-                    'Content-Type' in message and
-                    'text/quoted-printable' in message['Content-Type']):
-                  message.replace_header(
-                      'Content-Type', message['Content-Type'].replace(
-                          'text/quoted-printable', 'text/plain'))
-                  logging.info('Replaced text/quoted-printable with text/plain')
-              except (KeyboardInterrupt, SystemExit):
-                raise
-              except:
-                logging.exception(
-                    'Failed to replace text/quoted-printable with text/plain '
-                    'in Content-Type header')
-              try:
-                if args.fix_msgid and 'Message-ID' in message:
-                  msgid = message['Message-ID']
-                  if msgid[0] != '<':
-                    msgid = '<' + msgid
-                    logging.info('Added < to Message-ID: %s', msgid)
-                  if msgid[-1] != '>':
-                    msgid += '>'
-                    logging.info('Added > to Message-ID: %s', msgid)
-                  message.replace_header('Message-ID', msgid)
-              except (KeyboardInterrupt, SystemExit):
-                raise
-              except:
-                logging.exception('Failed to fix brackets in Message-ID header')
-              try:
-                metadata_object = {'labelIds': [label_id]}
-                # Use media upload to allow messages more than 5mb.
-                # See https://developers.google.com/api-client-library/python/guide/media_upload
-                # and http://google-api-python-client.googlecode.com/hg/docs/epy/apiclient.http.MediaIoBaseUpload-class.html.
-                message_data = io.BytesIO(message.as_string())
-                media = MediaIoBaseUpload(message_data,
-                                          mimetype='message/rfc822')
-                message_response = service.users().messages().import_(
-                    userId=username,
-                    fields='id',
-                    neverMarkSpam=True,
-                    processForCalendar=False,
-                    internalDateSource='dateHeader',
-                    body=metadata_object,
-                    media_body=media).execute(num_retries=args.num_retries)
-                logging.debug("Imported mbox message '%s' to Gmail ID %s",
-                              message.get_from(), message_response['id'])
-              except (KeyboardInterrupt, SystemExit):
-                raise
-              except:
-                logging.exception('Failed to import mbox message')
-            logging.info("Finished processing '%s'", full_filename)
-          else:
-            logging.info(
-                "Skipping '%s' because it doesn't have a .mbox extension",
-                full_filename)
-      except (KeyboardInterrupt, SystemExit):
+        result = process_mbox_files(username, service, labels)
+      except Exception:
+        logging.error("Can't process mbox files for user %s", username)
         raise
-      except:
-        logging.exception("Can't process mbox files for user %s", username)
-        raise
-      logging.info('Done importing user %s', username)
-    except (KeyboardInterrupt, SystemExit):
-      raise
-    except:
+      if result[2] == 0 and result[4] == 0:
+        number_of_users_imported_without_error += 1
+      elif result[0] > 0 or result[3] > 0:
+        number_of_users_imported_with_some_errors += 1
+      else:
+        number_of_users_failed += 1
+      number_of_labels_imported_without_error += result[0]
+      number_of_labels_imported_with_some_errors += result[1]
+      number_of_labels_failed += result[2]
+      number_of_messages_imported_without_error += result[3]
+      number_of_messages_failed += result[4]
+      logging.info('Done importing user %s. Labels: %d succeeded, %d with some '
+                   'errors, %d failed. Messages: %d succeeded, %d failed.',
+                   username,
+                   result[0],
+                   result[1],
+                   result[2],
+                   result[3],
+                   result[4])
+    except Exception:
+      number_of_users_failed += 1
       logging.exception("Can't process user %s", username)
-  logging.info("Done importing all users from directory '%s'", args.dir)
+  logging.info("*** Done importing all users from directory '%s'", args.dir)
+  logging.info('*** Import summary:')
+  logging.info('    %d users imported with no failures',
+               number_of_users_imported_without_error)
+  logging.info('    %d users imported with some failures',
+               number_of_users_imported_with_some_errors)
+  logging.info('    %d users failed',
+               number_of_users_failed)
+  logging.info('    %d labels (mbox files) imported with no failures',
+               number_of_labels_imported_without_error)
+  logging.info('    %d labels (mbox files) imported with some failures',
+               number_of_labels_imported_with_some_errors)
+  logging.info('    %d labels (mbox files) failed',
+               number_of_labels_failed)
+  logging.info('    %d messages imported successfully',
+               number_of_messages_imported_without_error)
+  logging.info('    %d messages failed\n',
+               number_of_messages_failed)
+  if (number_of_messages_failed + number_of_labels_failed +
+      number_of_users_failed > 0):
+    logging.info('*** Check log file %s for detailed errors.', args.log)
+  logging.info('Finished.\n\n')
 
 
 if __name__ == '__main__':
